eb9cf3ab42797cb95a1bc7e9b5c37d1f826ffd89
osm <osm0822@naver.com>
2023-06-29 17:57:32 +0900

    initial commit

---
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/BaGulBaGulApplication.java
@@ -0,0 +1,12 @@
+package com.BaGulBaGul.BaGulBaGul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaGulBaGulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaGulBaGulApplication.class, args);
+	}
+}
