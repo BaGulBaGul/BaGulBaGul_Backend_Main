284a9fcda66af61cfebfe12ff27ecc043a922bd1
osm <osm0822@naver.com>
2023-07-08 13:14:21 +0900

    add: Post, PostComment, PostCommentChild에 대한 user의 좋아요 기능 처리를 위한 entity 추가

---
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/post/PostLike.java
@@ -0,0 +1,42 @@
+package com.BaGulBaGul.BaGulBaGul.domain.post;
+
+import com.BaGulBaGul.BaGulBaGul.domain.post.PostLike.PostLikeId;
+import com.BaGulBaGul.BaGulBaGul.domain.user.User;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "post_like")
+@IdClass(PostLikeId.class)
+public class PostLike {
+    @EqualsAndHashCode
+    public class PostLikeId implements Serializable {
+        private Long post;
+        private Long user;
+    }
+
+    @Id
+    @JoinColumn(name = "post_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @Id
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    public PostLike(Post post, User user) {
+        this.post = post;
+        this.user = user;
+    }
+}
