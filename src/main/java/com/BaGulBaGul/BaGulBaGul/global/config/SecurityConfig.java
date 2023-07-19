105b42748db2461be2a7a0fbfd85b1c2a775fb0f
seheon <quwe153@naver.com>
2023-07-19 14:25:55 +0900

    refactor : 시큐리티 설정 변경

--- src/main/java/com/BaGulBaGul/BaGulBaGul/global/config/SecurityConfig.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/global/config/SecurityConfig.java
@@ -1,46 +1,46 @@
 package com.BaGulBaGul.BaGulBaGul.global.config;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    private static class AuthEntry {
-        private HttpMethod httpMethod;
-        private String path;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
-    /*
-        인증이 필요한 모든 경로를 적어주기
-        ex)new AuthEntry(HttpMethod.POST, "/api/post")
-     */
-    private static final AuthEntry[] needAuth = {
-    };
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
-                .csrf().disable()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
                 .httpBasic().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션x
+                .authorizeRequests()
+                .anyRequest().authenticated();
+                // JWT 및 로그인 필터 추가 예정
+                // 회원가입 및 로그인 예외 처리 EntryPoint 추가 예정
+        return http.build();
+    }
 
-        //needAuth에 있는 모든 method/path는 인증 필요
-        for(AuthEntry authEntry : needAuth) {
-            http.authorizeRequests().antMatchers(authEntry.httpMethod, authEntry.path).authenticated();
-        }
-        //그 외는 전부 허용
-        http.authorizeRequests().anyRequest().permitAll();
+    /* 스프링 시큐리티 룰을 무시하게 하는 Url 규칙(여기 등록하면 규칙 적용하지 않음) */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/doc", "/swagger*/**", "/favicon*/**", "/v2/api-docs"); // 스웨거 경로
+                // 인증이 필요하지 않는 경로 추가
     }
 }
