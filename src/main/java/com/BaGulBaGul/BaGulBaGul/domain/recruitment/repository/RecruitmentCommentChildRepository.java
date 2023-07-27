a54db0089c68ee9d44ed2ce5302395179fe3e7db
seheon <quwe153@naver.com>
2023-07-27 22:04:51 +0900

    feat : 모집글의 대댓글 조회

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/repository/RecruitmentCommentChildRepository.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/repository/RecruitmentCommentChildRepository.java
@@ -1,7 +1,14 @@
 package com.BaGulBaGul.BaGulBaGul.domain.recruitment.repository;
 
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.RecruitmentCommentChild;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecruitmentCommentChildRepository extends JpaRepository<RecruitmentCommentChild, Long> {
+
+    // 특정 댓글의 대댓글 조회
+    List<RecruitmentCommentChild> findCommentChildByRecruitmentCommentId(Long id, PageRequest pageRequest);
 }
