94ea6f0eb4863d2500b2efc18a49b8801b322611
seheon <quwe153@naver.com>
2023-07-27 22:00:41 +0900

    feat: 모집글 좋아요 도메인 생성

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/RecruitmentLike.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/RecruitmentLike.java
@@ -1,2 +1,40 @@
-package com.BaGulBaGul.BaGulBaGul.domain.recruitment;public class RecruitmentLike {
+package com.BaGulBaGul.BaGulBaGul.domain.recruitment;
+
+import com.BaGulBaGul.BaGulBaGul.domain.recruitment.RecruitmentLike.RecruitmentLikeId;
+import com.BaGulBaGul.BaGulBaGul.domain.user.User;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "recruitment_like")
+@IdClass(RecruitmentLikeId.class)
+public class RecruitmentLike {
+
+    @EqualsAndHashCode
+    public static class RecruitmentLikeId implements Serializable {
+        private Long recruitment;
+        private Long user;
+    }
+
+    @Id
+    @JoinColumn(name = "recruitment_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Recruitment recruitment;
+
+    @Id
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    public RecruitmentLike(Recruitment recruitment, User user) {
+        this.recruitment = recruitment;
+        this.user = user;
+    }
 }
--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/repository/RecruitmentLikeRepository.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/repository/RecruitmentLikeRepository.java
@@ -1,2 +1,10 @@
-package com.BaGulBaGul.BaGulBaGul.domain.recruitment.repository;public class RecruitmentLikeRepository {
+package com.BaGulBaGul.BaGulBaGul.domain.recruitment.repository;
+
+import com.BaGulBaGul.BaGulBaGul.domain.recruitment.RecruitmentLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruitmentLikeRepository extends JpaRepository<RecruitmentLike, Long> {
+
+    // 좋아요 개수 조회
+    Integer countByRecruitmentId(Long id);
 }
