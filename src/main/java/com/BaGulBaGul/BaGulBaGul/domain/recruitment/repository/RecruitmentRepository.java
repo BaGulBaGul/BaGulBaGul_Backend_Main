2ef153b48486df313c2dcf1798a85307589798d6
seheon <quwe153@naver.com>
2023-07-27 22:03:19 +0900

    feat : 모집글 단일 조회, 모집글 전체 조회 로직

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/repository/RecruitmentRepository.java
@@ -1,7 +1,24 @@
 package com.BaGulBaGul.BaGulBaGul.domain.recruitment.repository;
 
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.Recruitment;
+import com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.RecruitmentResponseDto;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+
+    // 모집글 단일 조회
+    Optional<Recruitment> findById(Long id);
+
+    // 특정 게시글의 모집글 조회 (정렬 기준 : (작성일, 좋아요)), dto 직접 조회
+    @Query("select new com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.RecruitmentResponseDto.RInfoWithPaging(r.id, r.user.imageURL, r.title, r.content, r.startDate, r.endDate, r.tags, r.createdAt, r.lastModifiedAt) " +
+            "from recruitment r " +
+            "join recruitment_like rl " +
+            "where r.post =: id and rl.recruitment = r group by r order by r.createdAt desc, count(r) desc")
+    List<RecruitmentResponseDto.RInfoWithPaging> findRecruitmentByPostId(Long id, PageRequest pageRequest);
+
 }
