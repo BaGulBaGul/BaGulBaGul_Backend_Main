991363f937d56c873f670ddb08d04546aa939360
seheon <quwe153@naver.com>
2023-07-27 22:04:01 +0900

    feat : 모집글 댓글 관련 쿼리 작성

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/repository/RecruitmentCommentRepository.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/repository/RecruitmentCommentRepository.java
@@ -1,7 +1,17 @@
 package com.BaGulBaGul.BaGulBaGul.domain.recruitment.repository;
 
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.RecruitmentComment;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecruitmentCommentRepository extends JpaRepository<RecruitmentComment, Long> {
+
+    // 모집글의 댓글 조회
+    List<RecruitmentComment> findCommentByRecruitmentId(Long id, PageRequest pageRequest);
+
+    // 모집글의 댓글 개수 조회
+    Integer countByRecruitmentId(Long id);
 }
