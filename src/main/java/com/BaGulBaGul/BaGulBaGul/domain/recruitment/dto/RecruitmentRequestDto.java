e719253a189ac918d9e473931b98f4ae1280acc3
seheon <quwe153@naver.com>
2023-07-27 22:01:48 +0900

    feat : 모집글 관련 dto 생성

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/dto/RecruitmentRequestDto.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/dto/RecruitmentRequestDto.java
@@ -1,2 +1,30 @@
-package com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto;public class RecruitmentRequestDto {
+package com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class RecruitmentRequestDto {
+    @Data @Builder
+    public static class RInfo {
+        private String title;
+
+        private String content;
+
+        private LocalDateTime startDate;
+
+        private LocalDateTime endDate;
+
+        private String tags;
+
+        private String imageURL;
+
+        private String category;
+
+        private Integer likeCount;
+
+        private Integer commentCount;
+    }
 }
--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/dto/RecruitmentResponseDto.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/dto/RecruitmentResponseDto.java
@@ -1,2 +1,60 @@
-package com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto;public class RecruitmentResponseDto {
+package com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto;
+
+import com.BaGulBaGul.BaGulBaGul.domain.base.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class RecruitmentResponseDto {
+    @Data @Builder
+    public static class RInfo {
+
+        private Long recruitmentId;
+
+        private String title;
+
+        private String content;
+
+        private LocalDateTime startDate;
+
+        private LocalDateTime endDate;
+
+        private String tags;
+
+        private String imageURL;
+
+        private String category;
+
+        private Integer likeCount;
+
+        private Integer commentCount;
+
+        private LocalDateTime createdAt;
+
+        private LocalDateTime lastModifiedAt;
+    }
+
+    @Data @Builder
+    public static class RInfoWithPaging{
+
+        private Long recruitmentId;
+
+        private String userImageURL;
+
+        private String title;
+
+        private String content;
+
+        private LocalDateTime startDate;
+
+        private LocalDateTime endDate;
+
+        private String tags;
+
+        private LocalDateTime createdAt;
+
+        private LocalDateTime lastModifiedAt;
+    }
 }
