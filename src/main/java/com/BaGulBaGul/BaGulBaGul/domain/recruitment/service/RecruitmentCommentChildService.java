65cfe010881bae7aaa3c8a08c21604f5a07e9854
seheon <quwe153@naver.com>
2023-08-08 10:37:13 +0900

    feat : 모집글 service단 작성

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/service/RecruitmentCommentChildService.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/service/RecruitmentCommentChildService.java
@@ -1,13 +1,39 @@
 package com.BaGulBaGul.BaGulBaGul.domain.recruitment.service;
 
+import com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.RecruitmentResponseDto;
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.repository.RecruitmentCommentChildRepository;
+import com.BaGulBaGul.BaGulBaGul.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class RecruitmentCommentChildService {
     private final RecruitmentCommentChildRepository recruitmentCommentChildRepository;
+
+    public ApiResponse<List<RecruitmentResponseDto.RCommentChildWithPaging>> getRecruitmentCommentChild(Long id, Integer offset){
+
+        List<RecruitmentResponseDto.RCommentChildWithPaging> request = new ArrayList<>();
+        PageRequest pageRequest = PageRequest.of(offset, 10);
+
+        // 작성자인지 판단하는 코드 추가 예정 (토큰으로 판단?)
+
+        recruitmentCommentChildRepository.findCommentChildByRecruitmentCommentId(id, pageRequest)
+                .forEach(r -> request.add(RecruitmentResponseDto.RCommentChildWithPaging
+                        .builder()
+                        .userImageURL(r.getUser().getImageURL())
+                        .content(r.getContent())
+                        .isWriter(false)
+                        .createdAt(r.getCreatedAt())
+                        .lastModifiedAt(r.getLastModifiedAt())
+                        .build()));
+
+        return ApiResponse.of(request);
+    }
 }
--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/service/RecruitmentService.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/service/RecruitmentService.java
@@ -1,18 +1,26 @@
 package com.BaGulBaGul.BaGulBaGul.domain.recruitment.service;
 
+import com.BaGulBaGul.BaGulBaGul.domain.post.Post;
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.Recruitment;
+import com.BaGulBaGul.BaGulBaGul.domain.recruitment.contant.RecruitmentType;
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.RecruitmentRequestDto;
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.RecruitmentResponseDto;
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.repository.RecruitmentCommentRepository;
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.repository.RecruitmentLikeRepository;
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.repository.RecruitmentRepository;
+import com.BaGulBaGul.BaGulBaGul.domain.user.User;
+import com.BaGulBaGul.BaGulBaGul.domain.user.repository.UserRepository;
 import com.BaGulBaGul.BaGulBaGul.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -23,26 +31,39 @@ public class RecruitmentService {
     private final RecruitmentRepository recruitmentRepository;
     private final RecruitmentCommentRepository recruitmentCommentRepository;
     private final RecruitmentLikeRepository recruitmentLikeRepository;
+    private final UserRepository userRepository;
 
     // 모집글 저장
     @Transactional
-    public ApiResponse save(RecruitmentRequestDto.RInfo requestDto){
+    public ApiResponse<Recruitment> save(RecruitmentRequestDto.RInfo requestDto){
+
+        // 예외 처리 예정
+        User user = userRepository.findById(requestDto.getUserId()).orElse(null);
 
         Recruitment recruitment = Recruitment.builder()
-                        // 개발 예정
-                        .build();
+                .type(requestDto.getType())
+                .user(user)
+                .title(requestDto.getTitle())
+                .content(requestDto.getContent())
+                .headCount(requestDto.getHeadCount())
+                .startDate(requestDto.getStartDate())
+                .endDate(requestDto.getEndDate())
+                .tags(requestDto.getTags())
+                .imageURL(requestDto.getImageURL())
+                .build();
+
         recruitmentRepository.save(recruitment);
         return ApiResponse.of(recruitment);
 
     }
 
     // 모집글 단일 조회
-    public RecruitmentResponseDto.RInfo getRecruitment(Long id){
+    public ApiResponse<RecruitmentResponseDto.RInfo> getRecruitment(Long id){
 
         // 예외 처리 예정
         Recruitment recruitment = recruitmentRepository.findById(id).orElse(null);
 
-        return RecruitmentResponseDto.RInfo.builder()
+        return ApiResponse.of(RecruitmentResponseDto.RInfo.builder()
                 .title(recruitment.getTitle())
                 .content(recruitment.getContent())
                 .tags(recruitment.getTags())
@@ -56,12 +77,27 @@ public class RecruitmentService {
                 // *****
                 .createdAt(recruitment.getCreatedAt())
                 .lastModifiedAt(recruitment.getLastModifiedAt())
-                .build();
+                .build());
     }
 
     // 모집글 전체 조회
-    public List<RecruitmentResponseDto.RInfoWithPaging> getRecruitments(Long id, Integer offset){
+    public ApiResponse<List<RecruitmentResponseDto.RInfoWithPaging>> getRecruitments(Long id, Integer offset){
+        List<RecruitmentResponseDto.RInfoWithPaging> request = new ArrayList<>();
         PageRequest pageRequest = PageRequest.of(offset, 20);
-        return recruitmentRepository.findRecruitmentByPostId(id, pageRequest);
+
+        recruitmentRepository.findRecruitmentByPostId(id, pageRequest)
+                .forEach(r -> request.add(RecruitmentResponseDto.RInfoWithPaging
+                        .builder()
+                        .recruitmentId(r.getId())
+                        .userImageURL(r.getUser().getImageURL())
+                        .title(r.getTitle())
+                        .content(r.getContent())
+                        .startDate(r.getStartDate())
+                        .endDate(r.getEndDate())
+                        .tags(r.getTags())
+                        .createdAt(r.getCreatedAt())
+                        .lastModifiedAt(r.getLastModifiedAt())
+                        .build()));
+        return ApiResponse.of(request);
     }
 }
