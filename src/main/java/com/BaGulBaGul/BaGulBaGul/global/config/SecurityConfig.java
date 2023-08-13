ec210cb909a6b42c0e4de5307b9bd7848a532a75
seheon <quwe153@naver.com>
2023-08-13 15:46:17 +0900

    feat : 시큐리티 설정

--- src/main/java/com/BaGulBaGul/BaGulBaGul/global/config/SecurityConfig.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/global/config/SecurityConfig.java
@@ -1,46 +1,46 @@
-//package com.BaGulBaGul.BaGulBaGul.global.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//public class SecurityConfig {
-//
-//    private final AuthenticationConfiguration authenticationConfiguration;
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.cors().and().csrf().disable()
-//                .httpBasic().disable()
-//                .sessionManagement(session ->
-//                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션x
-//                .authorizeRequests()
-//                .anyRequest().authenticated();
-//                // JWT 및 로그인 필터 추가 예정
-//                // 회원가입 및 로그인 예외 처리 EntryPoint 추가 예정
-//        return http.build();
-//    }
-//
-//    /* 스프링 시큐리티 룰을 무시하게 하는 Url 규칙(여기 등록하면 규칙 적용하지 않음) */
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring()
-//                .antMatchers("/doc", "/swagger*/**", "/favicon*/**", "/v2/api-docs"); // 스웨거 경로
-//                // 인증이 필요하지 않는 경로 추가
-//    }
-//}
+package com.BaGulBaGul.BaGulBaGul.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .httpBasic().disable()
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션x
+                .authorizeRequests()
+                .anyRequest().authenticated();
+                // JWT 및 로그인 필터 추가 예정
+                // 회원가입 및 로그인 예외 처리 EntryPoint 추가 예정
+        return http.build();
+    }
+
+    /* 스프링 시큐리티 룰을 무시하게 하는 Url 규칙(여기 등록하면 규칙 적용하지 않음) */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/doc", "/swagger*/**", "/favicon*/**", "/v2/api-docs"); // 스웨거 경로
+                // 인증이 필요하지 않는 경로 추가
+    }
+}
