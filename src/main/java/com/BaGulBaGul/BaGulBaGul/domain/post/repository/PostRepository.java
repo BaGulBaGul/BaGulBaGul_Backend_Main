e51c264682018fdf45521bcc9b51f40cef215a54
osm <osm0822@naver.com>
2023-08-14 18:28:21 +0900

    feat: PostRepository에 PostType과 userId로 좋아요 표시한 Post 검색 메서드 추가

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/post/repository/PostRepository.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/post/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.BaGulBaGul.BaGulBaGul.domain.post.repository;
 
 import com.BaGulBaGul.BaGulBaGul.domain.post.Post;
+import com.BaGulBaGul.BaGulBaGul.domain.post.constant.PostType;
 import java.util.Optional;
 
 import com.BaGulBaGul.BaGulBaGul.domain.post.repository.queryDSL.FindPostByCondition;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -21,4 +24,11 @@ public interface PostRepository extends JpaRepository<Post, Long>, FindPostByCon
     @Modifying(clearAutomatically = true)
     @Query(value = "update from Post post set post.likeCount = post.likeCount - 1 where post = :post")
     void decreaseLikeCount(@Param(value = "post") Post post);
+
+    @Query(
+            value = "SELECT p FROM Post p INNER JOIN p.likes pl WHERE p.type = :type and pl.user.id = :userId"
+    )
+    Page<Post> getLikePostWithType(
+            @Param("userId") Long userId, @Param("type") PostType type, Pageable pageable
+    );
 }
