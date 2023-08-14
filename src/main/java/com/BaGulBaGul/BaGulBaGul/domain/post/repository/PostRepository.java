3758c01c539006b945e4a7dfec59703afff51dc2
osm <osm0822@naver.com>
2023-08-14 16:11:40 +0900

    feat: 레포지토리에 좋아요 관련 메서드 추가

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/post/repository/PostRepository.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/post/repository/PostRepository.java
@@ -6,8 +6,19 @@ import java.util.Optional;
 import com.BaGulBaGul.BaGulBaGul.domain.post.repository.queryDSL.FindPostByCondition;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long>, FindPostByCondition {
     @EntityGraph(attributePaths = {"user", "categories.category"})
     Optional<Post> findWithUserAndCategoriesById(Long id);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "update from Post post set post.likeCount = post.likeCount + 1 where post = :post")
+    void increaseLikeCount(@Param(value = "post") Post post);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "update from Post post set post.likeCount = post.likeCount - 1 where post = :post")
+    void decreaseLikeCount(@Param(value = "post") Post post);
 }
