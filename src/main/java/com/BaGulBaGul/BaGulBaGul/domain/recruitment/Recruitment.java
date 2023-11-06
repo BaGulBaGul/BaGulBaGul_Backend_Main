a13087960b86509cfde478c82d9d23e9b3d1c3a4
osm <osm0822@naver.com>
2023-11-06 14:09:32 +0900

    refactor: 도메인 변경에 따라 recruitment 재정의

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/Recruitment.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/Recruitment.java
@@ -1,8 +1,8 @@
 package com.BaGulBaGul.BaGulBaGul.domain.recruitment;
 
 import com.BaGulBaGul.BaGulBaGul.domain.base.BaseTimeEntity;
+import com.BaGulBaGul.BaGulBaGul.domain.event.Event;
 import com.BaGulBaGul.BaGulBaGul.domain.post.Post;
-import com.BaGulBaGul.BaGulBaGul.domain.recruitment.contant.RecruitmentType;
 import com.BaGulBaGul.BaGulBaGul.domain.user.User;
 import lombok.*;
 import javax.persistence.*;
@@ -17,23 +17,13 @@ public class Recruitment extends BaseTimeEntity {
     @Column(name = "recruitment_id")
     Long id;
 
-    @Column(name = "type")
-    @Enumerated(value = EnumType.STRING)
-    RecruitmentType type;
-
-    @JoinColumn(name = "user_id")
-    @ManyToOne(fetch = FetchType.LAZY)
-    User user;
+    @JoinColumn(name = "event_id")
+    @OneToOne(fetch = FetchType.LAZY)
+    Event event;
 
     @JoinColumn(name = "post_id")
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     Post post;
-
-    @Column(name="title")
-    String title;
-
-    @Column(name="content")
-    String content;
 
     @Column(name="head_count")
     Integer headCount;
@@ -44,34 +34,18 @@ public class Recruitment extends BaseTimeEntity {
     @Column(name="end_date")
     LocalDateTime endDate;
 
-    @Column(name="tags")
-    String tags;
-
-    @Column(name="image_uri")
-    String imageURI;
-
     @Builder
     public Recruitment(
-            RecruitmentType type,
-            User user,
+            Event event,
             Post post,
-            String title,
-            String content,
             Integer headCount,
             LocalDateTime startDate,
-            LocalDateTime endDate,
-            String tags,
-            String imageURI
+            LocalDateTime endDate
     ){
-        this.type = type;
-        this.user = user;
+        this.event = event;
         this.post = post;
-        this.title = title;
-        this.content = content;
         this.headCount = headCount;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.tags = tags;
-        this.imageURI = imageURI;
     }
 }
--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/contant/RecruitmentType.java
+++
@@ -1,5 +0,0 @@
-package com.BaGulBaGul.BaGulBaGul.domain.recruitment.contant;
-
-public enum RecruitmentType {
-    FESTIVAL, LOCAL_EVENT
-}
