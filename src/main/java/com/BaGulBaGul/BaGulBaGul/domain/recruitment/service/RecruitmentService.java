3ead5ed1160f57912643e65d31b51e10b2b24116
osm <osm0822@naver.com>
2023-11-08 23:26:27 +0900

    feat: recruitment 관련 서비스 작성

---
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/service/RecruitmentService.java
@@ -0,0 +1,24 @@
+package com.BaGulBaGul.BaGulBaGul.domain.recruitment.service;
+
+import com.BaGulBaGul.BaGulBaGul.domain.post.exception.DuplicateLikeException;
+import com.BaGulBaGul.BaGulBaGul.domain.post.exception.LikeNotExistException;
+import com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.GetLikeRecruitmentResponse;
+import com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.RecruitmentConditionalRequest;
+import com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.RecruitmentDetailResponse;
+import com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.RecruitmentModifyRequest;
+import com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.RecruitmentRegisterRequest;
+import com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.RecruitmentSimpleResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface RecruitmentService {
+    RecruitmentDetailResponse getRecruitmentDetailById(Long recruitmentId);
+    Page<RecruitmentSimpleResponse> getRecruitmentPageByCondition(RecruitmentConditionalRequest recruitmentConditionalRequest, Pageable pageable);
+    Page<GetLikeRecruitmentResponse> getMyLikeRecruitment(Long userId, Pageable pageable);
+    Long registerRecruitment(Long eventId, Long userId, RecruitmentRegisterRequest recruitmentRegisterRequest);
+    void modifyRecruitment(Long recruitmentId, Long userId, RecruitmentModifyRequest recruitmentModifyRequest);
+    void deleteRecruitment(Long recruitmentId, Long userId);
+    void addLike(Long recruitmentId, Long userId) throws DuplicateLikeException;
+    void deleteLike(Long recruitmentId, Long userId) throws LikeNotExistException;
+    boolean isMyLike(Long recruitmentId, Long userId);
+}
