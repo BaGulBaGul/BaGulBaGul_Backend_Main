fa09d01aa1836abf4f1693cc8e8fdc1f3484f7e5
osm <osm0822@naver.com>
2023-11-09 00:15:29 +0900

    docs: swagger doc에 변경 사항을 반영

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/controller/RecruitmentControllerImpl.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/controller/RecruitmentControllerImpl.java
@@ -13,6 +13,7 @@ import com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto.RecruitmentSimpleRespons
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.service.RecruitmentService;
 import com.BaGulBaGul.BaGulBaGul.global.response.ApiResponse;
 import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -35,6 +36,9 @@ public class RecruitmentControllerImpl implements RecruitmentController {
 
     @Override
     @GetMapping("/recruitment/{recruitmentId}")
+    @Operation(summary = "모집글 id를 받아 자세한 정보를 조회",
+            description = "참고 : api 호출 시 조회수 1 증가"
+    )
     public ApiResponse<RecruitmentDetailResponse> getRecruitmentById(
             @PathVariable(name="recruitmentId") Long recruitmentId
     ) {
@@ -44,6 +48,14 @@ public class RecruitmentControllerImpl implements RecruitmentController {
 
     @Override
     @GetMapping("/{eventId}/recruitment")
+    @Operation(summary = "어떤 이벤트에 속하면서 주어진 조건에 맞는 모집글을 페이징 조회",
+            description = "페이징 관련 파라메터는 \n "
+                    + "http://localhost:8080/api/event/7/recruitment?size=10&page=0&sort=likeCount,desc \n "
+                    + "이런 식으로 넘기면 됨 \n "
+                    + "sort가 여러 개면 앞에서부터 순서대로 정렬 적용\n"
+                    + "파라메터로 명시하지 않은 조건은 무시되지만 페이징은 기본 페이징 조건 적용됨(page 0 size 20)\n"
+                    + "정렬 가능 속성 : createdAt, likeCount"
+    )
     public ApiResponse<Page<RecruitmentSimpleResponse>> getRecruitmentPageByCondition(
             @PathVariable(name = "eventId") Long eventId,
             RecruitmentConditionalApiRequest recruitmentConditionalApiRequest,
@@ -59,6 +71,10 @@ public class RecruitmentControllerImpl implements RecruitmentController {
 
     @Override
     @PostMapping("/{eventId}/recruitment")
+    @Operation(summary = "어떤 이벤트에 모집글을 등록하는 api",
+            description = "로그인 필요\n"
+                    + "생성한 모집글 id 반환"
+    )
     public ApiResponse<RecruitmentRegisterResponse> registerRecruitment(
             @PathVariable(name = "eventId") Long eventId,
             Long userId,
@@ -72,6 +88,10 @@ public class RecruitmentControllerImpl implements RecruitmentController {
 
     @Override
     @PatchMapping("/recruitment/{recruitmentId}")
+    @Operation(summary = "모집글 수정 api",
+            description = "로그인 필요\n"
+                    + "PATCH방식. 변경하기 싫은 필드는 보내지 않거나 null로 보내면 됨"
+    )
     public ApiResponse<Object> modifyRecruitment(
             @PathVariable(name="recruitmentId") Long recruitmentId,
             Long userId,
@@ -83,6 +103,9 @@ public class RecruitmentControllerImpl implements RecruitmentController {
 
     @Override
     @DeleteMapping("/recruitment/{recruitmentId}")
+    @Operation(summary = "모집글 삭제 api",
+            description = "로그인 필요"
+    )
     public ApiResponse<Object> deleteRecruitment(
             @PathVariable(name="recruitmentId") Long recruitmentId,
             Long userId
@@ -93,6 +116,11 @@ public class RecruitmentControllerImpl implements RecruitmentController {
 
     @Override
     @PostMapping("/recruitment/{recruitmentId}/like")
+    @Operation(summary = "모집글 좋아요 등록 api",
+            description = "로그인 필요\n"
+                    + "유저당 한번만 좋아요 등록 가능\n"
+                    + "이미 좋아요를 눌렀다면 무시됨"
+    )
     public ApiResponse<Object> addLike(
             @PathVariable(name="recruitmentId") Long recruitmentId,
             Long userId
@@ -106,6 +134,10 @@ public class RecruitmentControllerImpl implements RecruitmentController {
 
     @Override
     @DeleteMapping("/recruitment/{recruitmentId}/like")
+    @Operation(summary = "모집글 좋아요 삭제 api",
+            description = "로그인 필요\n"
+                    + "삭제할 좋아요가 없다면 무시됨"
+    )
     public ApiResponse<Object> deleteLike(
             @PathVariable(name="recruitmentId") Long recruitmentId,
             Long userId
@@ -119,6 +151,10 @@ public class RecruitmentControllerImpl implements RecruitmentController {
 
     @Override
     @GetMapping("/recruitment/{recruitmentId}/ismylike")
+    @Operation(summary = "모집글 좋아요 확인 api",
+            description = "로그인 필요\n"
+                    + "자신이 좋아요를 눌렀는지 boolean으로 확인"
+    )
     public ApiResponse<IsMyLikeResponse> isMyLike(
             @PathVariable(name="recruitmentId") Long recruitmentId,
             Long userId
@@ -130,6 +166,10 @@ public class RecruitmentControllerImpl implements RecruitmentController {
 
     @Override
     @GetMapping("/recruitment/mylike")
+    @Operation(summary = "자신이 좋아요를 누른 모든 모집글 검색",
+            description = "로그인 필요\n"
+                    + "페이징 지원"
+    )
     public ApiResponse<Page<GetLikeRecruitmentResponse>> getMyLike(
             Long userId,
             Pageable pageable
