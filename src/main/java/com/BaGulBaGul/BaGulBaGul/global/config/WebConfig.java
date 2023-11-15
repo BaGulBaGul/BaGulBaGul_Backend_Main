ee0c0438ac46d0bf4517d4a4aa1332a3b2fdac0d
osm <osm0822@naver.com>
2023-11-15 13:23:48 +0900

    feat: cookie에 담긴  token을 front의 요청에서 받아오기 위해 allow credentials 설정

--- src/main/java/com/BaGulBaGul/BaGulBaGul/global/config/WebConfig.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/global/config/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(FRONTEND_HOST)
+                .allowCredentials(true)
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
