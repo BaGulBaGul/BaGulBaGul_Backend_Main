7f2d4a97a2048efbba63d597256393bfacd57458
osm <osm0822@naver.com>
2023-11-30 19:57:36 +0900

    feat: resource upload service의 s3구현체 추가

---
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/global/upload/service/S3ResourceUploadService.java
@@ -0,0 +1,53 @@
+package com.BaGulBaGul.BaGulBaGul.global.upload.service;
+
+import com.BaGulBaGul.BaGulBaGul.global.upload.S3TempResource;
+import com.BaGulBaGul.BaGulBaGul.global.upload.repository.S3TempResourceRepository;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class S3ResourceUploadService extends ResourceUploadService{
+
+    private final AmazonS3 amazonS3;
+    private final S3TempResourceRepository s3TempResourceRepository;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Override
+    public String uploadResource(String path, MultipartFile multipartFile) throws IOException {
+        String key = createKey(path, multipartFile.getOriginalFilename());
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+        PutObjectResult qq;
+
+        qq = amazonS3.putObject(bucketName, key, multipartFile.getInputStream(), metadata);
+
+        s3TempResourceRepository.save(
+                S3TempResource.builder()
+                        .key(key)
+                        .uploadTime(LocalDateTime.now())
+                        .build()
+        );
+        return key;
+    }
+
+    @Override
+    public void deleteResource(String key) {
+        amazonS3.deleteObject(bucketName, key);
+    }
+
+    @Override
+    public String getResourceUrlFromKey(String key) {
+        return amazonS3.getUrl(bucketName, key).toString();
+    }
+}
