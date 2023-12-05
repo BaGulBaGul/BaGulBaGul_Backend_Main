ad6c73d06c1f64bf9b2fb6d979140b8c1f9a2d8c
osm <osm0822@naver.com>
2023-12-05 20:47:05 +0900

    feat: S3TempResource가 s3의 key가 아닌 Resource의 id를 FK,PK로 갖도록 변경

--- src/main/java/com/BaGulBaGul/BaGulBaGul/global/upload/S3TempResource.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/global/upload/S3TempResource.java
@@ -3,7 +3,10 @@ package com.BaGulBaGul.BaGulBaGul.global.upload;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,8 +21,9 @@ import lombok.Setter;
 @Entity
 public class S3TempResource {
     @Id
-    @Column(name = "resource_key")
-    private String key;
+    @JoinColumn(name = "resource_id")
+    @OneToOne(fetch = FetchType.LAZY)
+    private Resource resource;
 
     @Column(name = "upload_time")
     private LocalDateTime uploadTime;
