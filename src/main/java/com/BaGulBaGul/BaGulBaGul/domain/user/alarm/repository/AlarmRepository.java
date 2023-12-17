0f12b081705f0137f3f597abe9367d0f06288c58
osm <osm0822@naver.com>
2023-12-17 21:02:51 +0900

    feat: 필요한 Alarm Repository 메서드 추가

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/user/alarm/repository/AlarmRepository.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/user/alarm/repository/AlarmRepository.java
@@ -1,7 +1,25 @@
 package com.BaGulBaGul.BaGulBaGul.domain.user.alarm.repository;
 
 import com.BaGulBaGul.BaGulBaGul.domain.user.Alarm;
+import com.BaGulBaGul.BaGulBaGul.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+
+    @Query(
+            value = "SELECT al FROM Alarm al WHERE al.user = :user ORDER BY al.time DESC",
+            countQuery = "SELECT al FROM Alarm al WHERE al.user = :user"
+    )
+    Page<Alarm> findAlarmPageOrderByTime(@Param("user") User user, Pageable pageable);
+
+    @Modifying
+    @Query(
+            value = "DELETE FROM Alarm al WHERE al.user.id = :userId"
+    )
+    void deleteAllByUserId(@Param("userId") Long userId);
 }
