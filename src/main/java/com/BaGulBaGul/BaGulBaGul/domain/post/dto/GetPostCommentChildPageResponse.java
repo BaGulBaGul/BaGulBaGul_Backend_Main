1234a7b816aad40344f17e129ef6a1657e1d436f
osm <osm0822@naver.com>
2024-03-28 20:48:23 +0900

    feat: 대댓글 페이지 조회에서 작성자의 프로필 이미지 url을 응답하도록 api 수정

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/post/dto/GetPostCommentChildPageResponse.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/post/dto/GetPostCommentChildPageResponse.java
@@ -22,6 +22,9 @@ public class GetPostCommentChildPageResponse {
     @ApiModelProperty(value = "작성자 닉네임")
     private String userName;
 
+    @ApiModelProperty(value = "작성자 프로필 이미지 url")
+    private String userProfileImageUrl;
+
     @ApiModelProperty(value = "답장을 받는 유저 이름")
     private String replyTargetUserName;
 
