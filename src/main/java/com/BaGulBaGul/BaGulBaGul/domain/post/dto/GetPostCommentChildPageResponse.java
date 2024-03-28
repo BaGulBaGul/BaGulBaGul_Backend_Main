2b2f530204b09a07238ba02506485764d888c14e
osm <osm0822@naver.com>
2024-03-28 20:21:36 +0900

    feat: 대댓글 페이지 조회 시 답장인 경우 답장을 받는 유저의 닉네임을 함께 전송하도록 api 수정

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/post/dto/GetPostCommentChildPageResponse.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/post/dto/GetPostCommentChildPageResponse.java
@@ -22,6 +22,9 @@ public class GetPostCommentChildPageResponse {
     @ApiModelProperty(value = "작성자 닉네임")
     private String userName;
 
+    @ApiModelProperty(value = "답장을 받는 유저 이름")
+    private String replyTargetUserName;
+
     @ApiModelProperty(value = "내용")
     private String content;
 
