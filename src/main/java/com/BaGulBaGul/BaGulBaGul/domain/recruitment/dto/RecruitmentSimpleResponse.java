c28e474c15f92b7305cbed6b9d73cc094caee44f
osm <osm0822@naver.com>
2024-05-24 12:35:03 +0900

    refactor: RecruitmentSimpleResponse에서 모집글 정보와 게시글 정보를 객체화 해서 분리했다.

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/dto/RecruitmentSimpleResponse.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/recruitment/dto/RecruitmentSimpleResponse.java
@@ -1,5 +1,6 @@
 package com.BaGulBaGul.BaGulBaGul.domain.recruitment.dto;
 
+import com.BaGulBaGul.BaGulBaGul.domain.post.dto.PostSimpleInfo;
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.Recruitment;
 import com.BaGulBaGul.BaGulBaGul.domain.recruitment.constant.RecruitmentState;
 import io.swagger.annotations.ApiModelProperty;
@@ -18,62 +19,10 @@ import org.springframework.format.annotation.DateTimeFormat;
 @AllArgsConstructor
 public class RecruitmentSimpleResponse {
 
-    @ApiModelProperty(value = "모잡글 id")
-    private Long id;
+    @ApiModelProperty(value = "모잡글 정보")
+    private RecruitmentSimpleInfo recruitment;
 
-    @ApiModelProperty(value = "모집 상태")
-    private RecruitmentState state;
+    @ApiModelProperty(value = "게시글 정보")
+    private PostSimpleInfo post;
 
-    @ApiModelProperty(value = "참여 인원")
-    private Integer currentHeadCount;
-
-    @ApiModelProperty(value = "모집 인원")
-    private Integer totalHeadCount;
-
-    @ApiModelProperty(value = "시작 시간")
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime startDate;
-
-    @ApiModelProperty(value = "종료 시간")
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime endDate;
-
-    @ApiModelProperty(value = "게시글 제목")
-    private String title;
-
-    @ApiModelProperty(value = "등록자 id")
-    private Long userId;
-
-    @ApiModelProperty(value = "등록자 닉네임")
-    private String userName;
-
-    @ApiModelProperty(value = "등록자 이미지 url")
-    private String userProfileImageUrl;
-
-    @ApiModelProperty(value = "태그들", example = "[\"물놀이\",\"바베큐\"]")
-    private List<String> tags;
-
-    @ApiModelProperty(value = "생성일")
-    private LocalDateTime createdAt;
-
-    @ApiModelProperty(value = "마지막 수정일")
-    private LocalDateTime lastModifiedAt;
-
-    public static RecruitmentSimpleResponse of(Recruitment recruitment) {
-        return RecruitmentSimpleResponse.builder()
-                .id(recruitment.getId())
-                .state(recruitment.getState())
-                .currentHeadCount(recruitment.getCurrentHeadCount())
-                .totalHeadCount(recruitment.getTotalHeadCount())
-                .startDate(recruitment.getStartDate())
-                .endDate(recruitment.getEndDate())
-                .title(recruitment.getPost().getTitle())
-                .userId(recruitment.getPost().getUser().getId())
-                .userName(recruitment.getPost().getUser().getNickname())
-                .userProfileImageUrl(recruitment.getPost().getUser().getImageURI())
-                .tags(Arrays.asList(recruitment.getPost().getTags().split(" ")))
-                .createdAt(recruitment.getPost().getCreatedAt())
-                .lastModifiedAt(recruitment.getPost().getLastModifiedAt())
-                .build();
-    }
 }
