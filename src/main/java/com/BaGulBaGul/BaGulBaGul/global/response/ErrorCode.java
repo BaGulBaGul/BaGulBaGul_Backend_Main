4b9f7f2c452790be31d06f3fd1707163bff38ada
osm <osm0822@naver.com>
2024-06-22 01:50:50 +0900

    refactor: ErrorCode 열겨형을 클래스로 변경
    검증 예외 시 동적으로 생성되는 메세지를 담기 위함

--- src/main/java/com/BaGulBaGul/BaGulBaGul/global/response/ErrorCode.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/global/response/ErrorCode.java
@@ -8,38 +8,33 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-public enum ErrorCode {
+public class ErrorCode {
 
     //COMMON
-    SUCCESS("C00000", HttpStatus.OK,"SUCCESS"),
-    BAD_REQUEST("C10000", HttpStatus.BAD_REQUEST,"BAD REQUEST"),
-    UNAUTHORIZED("C10001", HttpStatus.UNAUTHORIZED, "UNAUTHORIZED"),
-    FORBIDDEN("C10002", HttpStatus.FORBIDDEN, "FORBIDDEN"),
-    NOT_FOUND("C10003", HttpStatus.NOT_FOUND, "PAGE NOT FOUND"),
-    INTERNAL_SERVER_ERROR("C20000", HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR"),
-
+    public static final ErrorCode SUCCESS = new ErrorCode("C00000", HttpStatus.OK,"SUCCESS");
+    public static final ErrorCode BAD_REQUEST = new ErrorCode("C10000", HttpStatus.BAD_REQUEST,"BAD REQUEST");
+    public static final ErrorCode UNAUTHORIZED = new ErrorCode("C10001", HttpStatus.UNAUTHORIZED, "UNAUTHORIZED");
+    public static final ErrorCode FORBIDDEN = new ErrorCode("C10002", HttpStatus.FORBIDDEN, "FORBIDDEN");
+    public static final ErrorCode NOT_FOUND = new ErrorCode("C10003", HttpStatus.NOT_FOUND, "PAGE NOT FOUND");
+    public static final ErrorCode INTERNAL_SERVER_ERROR = new ErrorCode("C20000", HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR");
     //EVENT
-    EVENT_NOT_FOUND("E00000", HttpStatus.OK, "이벤트가 존재하지 않습니다"),
-    EVENT_CATEGORY_NOT_EXIST("E00001", HttpStatus.OK, "존재하지 않는 카테고리입니다"),
-
+    public static final ErrorCode EVENT_NOT_FOUND = new ErrorCode("E00000", HttpStatus.OK, "이벤트가 존재하지 않습니다");
+    public static final ErrorCode EVENT_CATEGORY_NOT_EXIST = new ErrorCode("E00001", HttpStatus.OK, "존재하지 않는 카테고리입니다");
     //RECRUITMENT
-    RECRUITMENT_NOT_FOUND("R00000", HttpStatus.OK, "모집글이 존재하지 않습니다"),
-
+    public static final ErrorCode RECRUITMENT_NOT_FOUND = new ErrorCode("R00000", HttpStatus.OK, "모집글이 존재하지 않습니다");
     //POST
-    POST_NOT_FOUND("P00000", HttpStatus.OK, "게시글이 존재하지 않습니다"),
-
+    public static final ErrorCode POST_NOT_FOUND = new ErrorCode("P00000", HttpStatus.OK, "게시글이 존재하지 않습니다");
     //USER
-    USER_NOT_FOUND("U00000", HttpStatus.OK, "존재하지 않는 유저입니다"),
-
+    public static final ErrorCode USER_NOT_FOUND = new ErrorCode("U00000", HttpStatus.OK, "존재하지 않는 유저입니다");
     //ALARM
-    ALARM_NOT_FOUND("AL00000", HttpStatus.OK, "알람이 존재하지 않습니다"),
-
+    public static final ErrorCode ALARM_NOT_FOUND = new ErrorCode("AL00000", HttpStatus.OK, "알람이 존재하지 않습니다");
     //UserJoin
-    UJ_JOINTOKEN_WRONG("UJ00000", HttpStatus.OK, "소셜 로그인 회원가입 인증 토큰이 잘못되었습니다."),
-    UJ_JOINTOKEN_EXPIRED("UJ00001", HttpStatus.OK, "소셜 로그인 회원가입 인증 토큰이 만료되었습니다. 다시 인증해 주세요."),
-
+    public static final ErrorCode UJ_JOINTOKEN_WRONG = new ErrorCode("UJ00000", HttpStatus.OK, "소셜 로그인 회원가입 인증 토큰이 잘못되었습니다.");
+    public static final ErrorCode UJ_JOINTOKEN_EXPIRED = new ErrorCode("UJ00001", HttpStatus.OK, "소셜 로그인 회원가입 인증 토큰이 만료되었습니다. 다시 인증해 주세요.");
     //Upload
-    UPLOAD_NOT_IMAGE("UPLOAD00000", HttpStatus.OK, "이미지 파일이 아닙니다");
+    public static final ErrorCode UPLOAD_NOT_IMAGE = new ErrorCode("UPLOAD00000", HttpStatus.OK, "이미지 파일이 아닙니다");
+
+    public static final ErrorCode[] types = {SUCCESS, BAD_REQUEST};
 
     private final String code;
     private final HttpStatus httpStatus;
@@ -48,13 +43,10 @@ public enum ErrorCode {
     public static ErrorCode valueOf(HttpStatus httpStatus) {
         if (httpStatus == null) { throw new GeneralException(INTERNAL_SERVER_ERROR); }
 
-        return Arrays.stream(values())
-                .filter(errorCode -> errorCode.getHttpStatus() == httpStatus)
-                .findFirst()
-                .orElseGet(() -> {
-                    if (httpStatus.is4xxClientError()) { return ErrorCode.BAD_REQUEST; }
-                    else if (httpStatus.is5xxServerError()) { return ErrorCode.INTERNAL_SERVER_ERROR; }
-                    else { return ErrorCode.SUCCESS; }
-                });
+        if (httpStatus.is4xxClientError())
+            return ErrorCode.BAD_REQUEST;
+        else if (httpStatus.is5xxServerError())
+            return ErrorCode.INTERNAL_SERVER_ERROR;
+        return ErrorCode.SUCCESS;
     }
 }
