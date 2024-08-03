efbe42995bf06809cfe1238197467970153f5be7
osm <osm0822@naver.com>
2024-08-03 12:22:43 +0900

    feat: 카테고리 정보를 flyway init 시에 넣도록 변경

---
+++ src/main/resources/db/migration/V4__Init_Category.sql
@@ -0,0 +1,5 @@
+INSERT INTO category values(null, "문화/예술");
+INSERT INTO category values(null, "공연전시/행사");
+INSERT INTO category values(null, "식품/음료");
+INSERT INTO category values(null, "교육/체험");
+INSERT INTO category values(null, "스포츠/레저");
