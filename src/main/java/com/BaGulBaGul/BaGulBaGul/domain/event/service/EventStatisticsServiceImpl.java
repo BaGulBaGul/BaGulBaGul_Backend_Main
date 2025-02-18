3df0cbc03ab4b10f50ae10a9f4b911baee54d564
osm <osm0822@naver.com>
2025-02-18 14:20:24 +0900

    feat: 사용자가 이벤트 상세조회 요청 시 이벤트 랭킹에 반영

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/event/service/EventStatisticsServiceImpl.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/event/service/EventStatisticsServiceImpl.java
@@ -1,8 +1,10 @@
 package com.BaGulBaGul.BaGulBaGul.domain.event.service;
 
+import com.BaGulBaGul.BaGulBaGul.domain.event.constant.EventType;
 import com.BaGulBaGul.BaGulBaGul.domain.event.dto.service.response.EventDetailResponse;
-import com.BaGulBaGul.BaGulBaGul.domain.event.repository.EventRepository;
+import com.BaGulBaGul.BaGulBaGul.domain.event.repository.EventViewRankingRepository;
 import com.BaGulBaGul.BaGulBaGul.domain.post.service.PostStatisticsService;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,32 @@ import org.springframework.stereotype.Service;
 public class EventStatisticsServiceImpl implements EventStatisticsService {
 
     private final PostStatisticsService postStatisticsService;
+    private final EventViewRankingRepository eventViewRankingRepository;
 
     @Override
     @Async
     public void handleViewByUser(EventDetailResponse eventDetailResponse) {
+        //post 관련 통계 처리를 위임
         postStatisticsService.handleViewByUser(eventDetailResponse.getPost());
+        //이벤트 조회수 실시간 랭킹 반영
+        Long eventId = eventDetailResponse.getEvent().getEventId();
+        EventType eventType = eventDetailResponse.getEvent().getType();
+        applyToRealtimeEventViewRanking(eventId, eventType);
+    }
+
+    private void applyToRealtimeEventViewRanking(Long eventId, EventType eventType) {
+        //오늘 날짜의 조회수 증가
+        eventViewRankingRepository.increaseDayViewCount(
+                eventId,
+                eventType,
+                LocalDateTime.now(),
+                1L
+        );
+        //최근 7일간의 조회수 증가
+        eventViewRankingRepository.increase7DaysViewCount(
+                eventId,
+                eventType,
+                1L
+        );
     }
 }
