9e3eb6f1ed030ecac073d8318d863e6665ccaca8
osm <osm0822@naver.com>
2025-02-24 15:45:05 +0900

    chore: 스프링 배치 설정 추가

---
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/global/config/SchedulerConfig.java
@@ -0,0 +1,13 @@
+package com.BaGulBaGul.BaGulBaGul.global.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@ConditionalOnProperty(
+        value = "app.scheduling.enable", havingValue = "true", matchIfMissing = true
+)   
+@EnableScheduling
+public class SchedulerConfig {
+}
