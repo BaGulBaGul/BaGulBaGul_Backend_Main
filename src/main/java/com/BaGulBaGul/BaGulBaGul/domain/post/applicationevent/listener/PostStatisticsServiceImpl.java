3417b7eb85e9bd53b333f7e246bcb70e1c146774
osm <osm0822@naver.com>
2025-02-26 12:15:49 +0900

    feat: 게시글 조회 시 태그 랭킹에 반영

--- src/main/java/com/BaGulBaGul/BaGulBaGul/domain/post/applicationevent/listener/PostStatisticsServiceImpl.java
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/post/applicationevent/listener/PostStatisticsServiceImpl.java
@@ -2,6 +2,7 @@ package com.BaGulBaGul.BaGulBaGul.domain.post.applicationevent.listener;
 
 import com.BaGulBaGul.BaGulBaGul.domain.post.applicationevent.QueryPostDetailByUserApplicationEvent;
 import com.BaGulBaGul.BaGulBaGul.domain.post.repository.PostRepository;
+import com.BaGulBaGul.BaGulBaGul.domain.ranking.service.TagRealtimeRankingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostStatisticsServiceImpl implements PostStatisticsService {
 
     private final PostRepository postRepository;
+    private final TagRealtimeRankingService tagRealtimeRankingService;
 
     @Override
     @EventListener
     @Transactional
     public void handleQueryDetailByUser(QueryPostDetailByUserApplicationEvent event) {
+        //조회수 증가
         postRepository.increaseViewsById(event.getPostDetailInfo().getPostId());
+        //태그 랭킹 반영
+        tagRealtimeRankingService.increaseTagScore1(event.getPostDetailInfo().getTags());
     }
 }
