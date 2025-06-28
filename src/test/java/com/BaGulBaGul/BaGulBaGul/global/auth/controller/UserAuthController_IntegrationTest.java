e65b6440b51dd5f2be7eca1b1d5e3c0abe04a424
osm <osm0822@naver.com>
2025-06-28 20:20:43 +0900

    test: 테스트 추가

---
+++ src/test/java/com/BaGulBaGul/BaGulBaGul/global/auth/controller/UserAuthController_IntegrationTest.java
@@ -0,0 +1,167 @@
+package com.BaGulBaGul.BaGulBaGul.global.auth.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.BaGulBaGul.BaGulBaGul.extension.AllTestContainerExtension;
+import com.BaGulBaGul.BaGulBaGul.global.auth.service.JwtProvider;
+import com.BaGulBaGul.BaGulBaGul.global.response.ResponseCode;
+import java.util.Calendar;
+import java.util.Date;
+import javax.servlet.http.Cookie;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(AllTestContainerExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class UserAuthController_IntegrationTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Value("${user.login.access_token_cookie_name}")
+    private String ACCESS_TOKEN_COOKIE_NAME;
+
+    @Value("${user.login.refresh_token_cookie_name}")
+    private String REFRESH_TOKEN_COOKIE_NAME;
+
+    @Value("${user.login.refresh_token_expire_minute}")
+    private int REFRESH_TOKEN_EXPIRE_MINUTE;
+
+    @BeforeEach
+    void setup(WebApplicationContext ctx) {
+        mvc = MockMvcBuilders.webAppContextSetup(ctx)
+                .apply(springSecurity())
+                .defaultRequest(get("/").with(csrf()))
+                .build();
+    }
+
+    @Nested
+    @DisplayName("인증 토큰 재발급 테스트")
+    class RefreshTokenTest {
+        @Test
+        @DisplayName("정상 AT, 정상 RT => 403 FORBIDDEN")
+        void shouldRefresh_WhenNormalATNormalRT() throws Exception {
+            //given
+            Long userId = 1L;
+            String accessToken = jwtProvider.createAccessToken(userId);
+            String refreshToken = jwtProvider.createRefreshToken(userId);
+
+            int rtExpireSecond = REFRESH_TOKEN_EXPIRE_MINUTE * 60;
+            int atExpireSecond = rtExpireSecond;
+
+            //when then
+            ResponseCode responseCode = ResponseCode.FORBIDDEN;
+            mvc.perform(post("/api/auth/refresh")
+                            .cookie(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken))
+                            .cookie(new Cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken)))
+                    .andExpect(status().is(responseCode.getHttpStatus().value()))
+                    .andExpect(jsonPath("$.errorCode").value(responseCode.getCode()));
+        }
+
+        @Test
+        @DisplayName("만료된 AT, 정상 RT => 200 SUCCESS")
+        void shouldRefresh_WhenExpiredATNormalRT() throws Exception {
+            //given
+            Long userId = 1L;
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(new Date());
+            calendar.add(Calendar.MINUTE, -10);
+            Date expiredDate = calendar.getTime();
+
+            String accessToken = jwtProvider.createAccessToken(userId, expiredDate, expiredDate);
+            String refreshToken = jwtProvider.createRefreshToken(userId);
+
+            int rtExpireSecond = REFRESH_TOKEN_EXPIRE_MINUTE * 60;
+            int atExpireSecond = rtExpireSecond;
+
+            //when then
+            ResponseCode responseCode = ResponseCode.SUCCESS;
+            mvc.perform(post("/api/auth/refresh")
+                            .cookie(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken))
+                            .cookie(new Cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken)))
+                    .andExpect(status().is(responseCode.getHttpStatus().value()))
+                    .andExpect(jsonPath("$.errorCode").value(responseCode.getCode()))
+                    .andExpectAll(
+                            cookie().exists(ACCESS_TOKEN_COOKIE_NAME),
+                            cookie().path(ACCESS_TOKEN_COOKIE_NAME, "/"),
+                            cookie().maxAge(ACCESS_TOKEN_COOKIE_NAME, atExpireSecond))
+                    .andExpectAll(
+                            cookie().exists(REFRESH_TOKEN_COOKIE_NAME),
+                            cookie().path(REFRESH_TOKEN_COOKIE_NAME, "/api/auth/refresh"),
+                            cookie().maxAge(REFRESH_TOKEN_COOKIE_NAME, rtExpireSecond));
+
+        }
+
+        @Test
+        @DisplayName("정상 AT, 만료된 RT =? 403 FORBIDDEN")
+        void shouldNotRefresh_WhenNormalATExpiredRT() throws Exception {
+            //given
+            Long userId = 1L;
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(new Date());
+            calendar.add(Calendar.MINUTE, -10);
+            Date expiredDate = calendar.getTime();
+
+            String accessToken = jwtProvider.createAccessToken(userId);
+            String refreshToken = jwtProvider.createRefreshToken(userId, expiredDate, expiredDate);
+
+            //when then
+            ResponseCode responseCode = ResponseCode.FORBIDDEN;
+            mvc.perform(post("/api/auth/refresh")
+                            .cookie(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken))
+                            .cookie(new Cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken)))
+                    .andExpect(status().is(responseCode.getHttpStatus().value()))
+                    .andExpect(jsonPath("$.errorCode").value(responseCode.getCode()));
+        }
+
+
+        @Test
+        @DisplayName("만료된 AT, 만료된 RT => 401 AUTH_EXPIRED_REFRESH_TOKEN")
+        void shouldNotRefresh_WhenExpiredATExpiredRT() throws Exception {
+            //given
+            Long userId = 1L;
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(new Date());
+            calendar.add(Calendar.MINUTE, -10);
+            Date expiredDate = calendar.getTime();
+
+            String accessToken = jwtProvider.createAccessToken(userId, expiredDate, expiredDate);
+            String refreshToken = jwtProvider.createRefreshToken(userId, expiredDate, expiredDate);
+
+            //when
+            ResponseCode responseCode = ResponseCode.AUTH_EXPIRED_REFRESH_TOKEN;
+            ResultActions perform = mvc.perform(post("/api/auth/refresh")
+                    .cookie(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken))
+                    .cookie(new Cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken)));
+
+            //then
+            perform.andExpect(status().is(responseCode.getHttpStatus().value()))
+                    .andExpect(jsonPath("$.errorCode").value(responseCode.getCode()));
+        }
+    }
+}
---
+++ src/test/java/com/BaGulBaGul/BaGulBaGul/global/auth/filter/JwtAuthenticationFilter_IntegrationTest.java
@@ -0,0 +1,160 @@
+package com.BaGulBaGul.BaGulBaGul.global.auth.filter;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.BaGulBaGul.BaGulBaGul.domain.user.User;
+import com.BaGulBaGul.BaGulBaGul.domain.user.sampledata.UserSample;
+import com.BaGulBaGul.BaGulBaGul.domain.user.service.UserJoinService;
+import com.BaGulBaGul.BaGulBaGul.extension.AllTestContainerExtension;
+import com.BaGulBaGul.BaGulBaGul.global.auth.service.JwtProvider;
+import com.BaGulBaGul.BaGulBaGul.global.response.ApiResponse;
+import com.BaGulBaGul.BaGulBaGul.global.response.ResponseCode;
+import java.util.Calendar;
+import java.util.Date;
+import javax.servlet.http.Cookie;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(AllTestContainerExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class JwtAuthenticationFilter_IntegrationTest {
+
+    public static final String SECURE_PATH = "/api/user/info/my";
+    public static final String NORMAL_PATH = "/";
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Autowired
+    UserJoinService userJoinService;
+
+    @Value("${user.login.access_token_cookie_name}")
+    private String ACCESS_TOKEN_COOKIE_NAME;
+
+    @BeforeEach
+    void setup(WebApplicationContext ctx) {
+        mvc = MockMvcBuilders.webAppContextSetup(ctx)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @DisplayName("인증이 필요 없는 api에 access token 없이 요청 => 200")
+    @Transactional
+    void shouldOk_WhenNotNeedAuthenticate_WhenNotOfferAT() throws Exception {
+        //given
+        Long userId = 1L;
+        String accessToken = jwtProvider.createAccessToken(userId);
+
+        mvc.perform(get(NORMAL_PATH)
+                        .cookie(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("인증이 필요 없는 api에 정상 access token => 200")
+    @Transactional
+    void shouldOk_WhenNotNeedAuthenticate_WhenOfferNormalAT() throws Exception {
+        mvc.perform(get(NORMAL_PATH))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("인증이 필요한 api에 정상 access token => 200")
+    @Transactional
+    void shouldOk_WhenNeedAuthenticate_WhenOfferNormalAT() throws Exception {
+        //given
+        User user = userJoinService.registerUser(UserSample.NORMAL_USER_REGISTER_REQUEST);
+        Long userId = user.getId();
+        String accessToken = jwtProvider.createAccessToken(userId);
+
+        mvc.perform(get(SECURE_PATH)
+                .cookie(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.errorCode").value(ResponseCode.SUCCESS.getCode()));
+    }
+
+    @Test
+    @DisplayName("인증이 필요한 api에 만료된 access token => 401 AUTH_EXPIRED_ACCESS_TOKEN")
+    @Transactional
+    void shouldResponseAUTH_EXPIRED_ACCESS_TOKEN_WhenNeedAuthenticate_WhenOfferTamperedAT() throws Exception {
+        //given
+        User user = userJoinService.registerUser(UserSample.NORMAL_USER_REGISTER_REQUEST);
+        Long userId = user.getId();
+        Date now = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(now);
+        calendar.add(Calendar.MINUTE, -20);
+        Date issuedAt = calendar.getTime();
+        calendar.add(Calendar.MINUTE, 15);
+        Date expireAt = calendar.getTime();
+        String accessToken = jwtProvider.createAccessToken(userId, issuedAt, expireAt);
+
+        //when
+        mvc.perform(get(SECURE_PATH)
+                        .cookie(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken)))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.errorCode").value(ResponseCode.AUTH_EXPIRED_ACCESS_TOKEN.getCode()));
+    }
+
+    @Test
+    @DisplayName("인증이 필요한 api에 변조된 access token => 401 UNAUTHORIZED")
+    @Transactional
+    void shouldResponseUNAUTHORIZED_WhenNeedAuthenticate_WhenOfferTamperedAT() throws Exception {
+        //given
+        User user = userJoinService.registerUser(UserSample.NORMAL_USER_REGISTER_REQUEST);
+        Long userId = user.getId();
+        String accessToken = jwtProvider.createAccessToken(userId);
+        //서명 마지막 문자만 변조
+        String b64urlChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "abcdefghijklmnopqrstuvwxyz" +
+                "0123456789" +
+                "-_";
+        char last = accessToken.charAt(accessToken.length() - 1);
+        char newLast = b64urlChars
+                .chars()
+                .mapToObj(c -> (char) c)
+                .filter(c -> c != last)    // 기존 문자와 다르게
+                .findFirst()
+                .orElseThrow();
+        accessToken = accessToken.substring(0, accessToken.length() - 1) + newLast;
+
+        //when
+        mvc.perform(get(SECURE_PATH)
+                        .cookie(new Cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken)))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.errorCode").value(ResponseCode.UNAUTHORIZED.getCode()));
+    }
+}
