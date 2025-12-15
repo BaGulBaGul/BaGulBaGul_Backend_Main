d40692e0871dd4bab7e50e2141036cd3091a07be
osm <osm0822@naver.com>
2025-12-15 16:06:38 +0900

    feat: Role을 관리하는 관리자 api 추가
    Role에 대한 조회, 삭제, 권한 변경 등을 수행

---
+++ src/main/java/com/BaGulBaGul/BaGulBaGul/domain/admin/user/dto/api/request/RoleRegisterApiRequest.java
@@ -0,0 +1,24 @@
+package com.BaGulBaGul.BaGulBaGul.domain.admin.user.dto.api.request;
+
+import com.BaGulBaGul.BaGulBaGul.global.auth.constant.PermissionType;
+import com.BaGulBaGul.BaGulBaGul.global.auth.dto.RoleRegisterRequest;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleRegisterApiRequest {
+    String roleName;
+    public RoleRegisterRequest toRoleRegisterRequest() {
+        return RoleRegisterRequest.builder()
+                .roleName(roleName)
+                .build();
+    }
+}
